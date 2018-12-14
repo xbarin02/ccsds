d87b645c0a833df84f6dd1087a7735c1e93b96cf
David Barina <ibarina@fit.vutbr.cz>
2018-12-14 15:59:05 +0100

    get rid of the <math.h> and -lm

--- phase0/compress.c
+++ phase0/compress.c
@@ -17,10 +17,27 @@
 #include <stdlib.h>
 #include <limits.h>
 
-/* floor */
-#include <math.h>
+static int floor_(double x)
+{
+	/*
+	 * per C89 standard, 6.2.1.3 Floating and integral:
+	 *
+	 * When a value of floating type is convened to integral type,
+	 * the fractional part is discarded. If the value of the integral part
+	 * cannot be represented by the integral type, the behavior is
+	 * undetined.
+	 *
+	 * "... discarded", i.e., the value is truncated toward zero
+	 */
 
-#define round_(x) floor( (x) + 0.5 )
+	/* truncate */
+	int i = (int) x;
+
+	/* convert trunc to floor */
+	return i - (int) ( (double) i > x );
+}
+
+#define round_(x) floor_( (x) + 0.5 )
 
 /**
  * error codes
