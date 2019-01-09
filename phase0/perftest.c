c4a948ff684a3e8396f5ef113507ab11ae55835d
David Barina <ibarina@fit.vutbr.cz>
2019-01-09 23:01:09 +0100

    measurement collected out into func

--- phase0/perftest.c
+++ phase0/perftest.c
@@ -6,35 +6,43 @@
 #include "common.h"
 #include "dwt.h"
 
-int main()
+double measure_dwt_encode_secs(struct frame *frame)
 {
-	struct frame frame;
 	struct parameters parameters;
-
 	clock_t begin, end;
-	double t;
 
-	frame.width = 4096;
-	frame.height = 2048;
-	frame.bpp = 16;
-
-	if (frame_create_random(&frame)) {
+	if (frame_create_random(frame)) {
 		fprintf(stderr, "[ERROR] frame allocation failed\n");
-		return EXIT_FAILURE;
+		return 0.;
 	}
 
 	parameters.DWTtype = 0;
 
 	begin = clock();
 
-	if (dwt_encode(&frame, &parameters)) {
+	if (dwt_encode(frame, &parameters)) {
 		fprintf(stderr, "[ERROR] transform failed\n");
-		return EXIT_FAILURE;
+		return 0.;
 	}
 
 	end = clock();
 
-	t = (double)(end - begin) / CLOCKS_PER_SEC;
+	frame_destroy(frame);
+
+	return (double)(end - begin) / CLOCKS_PER_SEC;
+}
+
+int main()
+{
+	struct frame frame;
+
+	double t;
+
+	frame.width = 4096;
+	frame.height = 2048;
+	frame.bpp = 16;
+
+	t = measure_dwt_encode_secs(&frame);
 
 	printf("%f secs\n", t);
 
