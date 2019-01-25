2aa4a982a8bc42f8e14c2b882fde542ab4101a06
David Barina <ibarina@fit.vutbr.cz>
2019-01-25 13:01:02 +0100

    makeplot update

--- phase0/makeplot.sh
+++ phase0/makeplot.sh
@@ -1,4 +1,6 @@
-#!/bin/sh
+#!/bin/bash
+
+set -e -u
 
 # sed "s/^\s*#\s*define\s*CONFIG_PERFTEST_TYPE\s*[0-9]\+\s*$/#define CONFIG_PERFTEST_TYPE 1/" config.h
 function config
@@ -11,12 +13,12 @@ config CONFIG_PERFTEST_TYPE 0
 config CONFIG_PERFTEST_NUM 16
 
 make clean
-make EXTRA_CFLAGS=-fprofile-generate EXTRA_LDLIBS=-lgcov
+make perftest EXTRA_CFLAGS=-fprofile-generate EXTRA_LDLIBS=-lgcov
 
 ./perftest
 
 config CONFIG_PERFTEST_NUM 64
 make clean
-make EXTRA_CFLAGS=-fprofile-use
+make perftest EXTRA_CFLAGS=-fprofile-use
 
 ./perftest
