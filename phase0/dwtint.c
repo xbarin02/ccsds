a5d62443290e2e18e731eccd3bc26beab7d8bada
David Barina <ibarina@fit.vutbr.cz>
2019-04-26 14:12:44 +0200

    dwtint: dwtint_decode_block

--- phase0/dwtint.c
+++ phase0/dwtint.c
@@ -633,6 +633,31 @@ void dwtint_encode_block(int *data, ptrdiff_t stride_y[3], ptrdiff_t stride_x[3]
 	}
 }
 
+/* process 8x8 block using multi-scale transform */
+void dwtint_decode_block(int *data, ptrdiff_t stride_y[3], ptrdiff_t stride_x[3], ptrdiff_t height[3], ptrdiff_t width[3], int *buff_y[3], int *buff_x[3], ptrdiff_t y, ptrdiff_t x)
+{
+	ptrdiff_t y_, x_;
+
+	/* j = 2 */
+	for (y_ = y/8; y_ < y/8+1; ++y_) {
+		for (x_ = x/8; x_ < x/8+1; ++x_) {
+			dwtint_decode_quad(data, height[2], width[2], stride_y[2], stride_x[2], buff_y[2], buff_x[2], y_ - 0, x_ - 0);
+		}
+	}
+	/* j = 1 */
+	for (y_ = y/4; y_ < y/4+2; ++y_) {
+		for (x_ = x/4; x_ < x/4+2; ++x_) {
+			dwtint_decode_quad(data, height[1], width[1], stride_y[1], stride_x[1], buff_y[1], buff_x[1], y_ - 3, x_ - 3);
+		}
+	}
+	/* j = 0 */
+	for (y_ = y/2; y_ < y/2+4; ++y_) {
+		for (x_ = x/2; x_ < x/2+4; ++x_) {
+			dwtint_decode_quad(data, height[0], width[0], stride_y[0], stride_x[0], buff_y[0], buff_x[0], y_ - 10, x_ - 10);
+		}
+	}
+}
+
 /* process strip using multi-scale transform */
 void dwtint_encode_strip(int *data, ptrdiff_t stride_y[3], ptrdiff_t stride_x[3], ptrdiff_t height[3], ptrdiff_t width[3], int *buff_y[3], int *buff_x[3], ptrdiff_t y)
 {
@@ -875,6 +900,29 @@ int dwtint_decode(struct frame *frame)
 
 	for (y = 0; y < height+24; y += 8) {
 		dwtint_decode_strip(data, stride_y_, stride_x_, height_, width_, buff_y_, buff_x_, y);
+	}
+
+	for (j = 0; j < 3; ++j) {
+		free(buff_y_[j]);
+		free(buff_x_[j]);
+	}
+#endif
+#if (CONFIG_DWT_MS_MODE == 2)
+	for (j = 0; j < 3; ++j) {
+		height_[j] = (height >> j) >> 1;
+		width_ [j] = (width  >> j) >> 1;
+
+		stride_y_[j] = width << j;
+		stride_x_[j] =     1 << j;
+
+		buff_y_[j] = malloc( (size_t) (2 * height_[j] + (32 >> j) - 2) * 5 * sizeof(int) );
+		buff_x_[j] = malloc( (size_t) (2 * width_ [j] + (32 >> j) - 2) * 5 * sizeof(int) );
+	}
+
+	for (y = 0; y < height+24; y += 8) {
+		for (x = 0; x < width+24; x += 8) {
+			dwtint_decode_block(data, stride_y_, stride_x_, height_, width_, buff_y_, buff_x_, y, x);
+		}
 	}
 
 	for (j = 0; j < 3; ++j) {
