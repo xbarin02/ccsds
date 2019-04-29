12fe3b0cc1b8dc45b761625856601437b9ff41b0
David Barina <ibarina@fit.vutbr.cz>
2019-04-29 21:18:25 +0200

    bpe: skeleton funcs

--- phase0/bpe.c
+++ phase0/bpe.c
@@ -1,16 +1,72 @@
 #include "bpe.h"
 #include <assert.h>
 
+int bpe_encode_block(int *data, ptrdiff_t stride, struct bio *bio)
+{
+	ptrdiff_t y, x;
+
+	for (y = 0; y < 8; ++y) {
+		for (x = 0; x < 8; ++x) {
+			bio_write_int(bio, data[y*stride + x]);
+		}
+	}
+
+	return 0;
+}
+
+int bpe_decode_block(int *data, ptrdiff_t stride, struct bio *bio)
+{
+	ptrdiff_t y, x;
+
+	for (y = 0; y < 8; ++y) {
+		for (x = 0; x < 8; ++x) {
+			bio_read_int(bio, &data[y*stride + x]);
+		}
+	}
+
+	return 0;
+}
+
 int bpe_encode(struct frame *frame, const struct parameters *parameters, struct bio *bio)
 {
+	ptrdiff_t height, width;
+	ptrdiff_t y, x;
+	int *data;
+
 	assert(frame);
+
+	height = (ptrdiff_t) ceil_multiple8(frame->height);
+	width  = (ptrdiff_t) ceil_multiple8(frame->width);
+
+	data = frame->data;
+
+	for (y = 0; y < height; y += 8) {
+		for (x = 0; x < width; x += 8) {
+			bpe_encode_block(data + y*width + x, width, bio);
+		}
+	}
 
 	return 0;
 }
 
 int bpe_decode(struct frame *frame, const struct parameters *parameters, struct bio *bio)
 {
+	ptrdiff_t height, width;
+	ptrdiff_t y, x;
+	int *data;
+
 	assert(frame);
+
+	height = (ptrdiff_t) ceil_multiple8(frame->height);
+	width  = (ptrdiff_t) ceil_multiple8(frame->width);
+
+	data = frame->data;
+
+	for (y = 0; y < height; y += 8) {
+		for (x = 0; x < width; x += 8) {
+			bpe_decode_block(data + y*width + x, width, bio);
+		}
+	}
 
 	return 0;
 }
