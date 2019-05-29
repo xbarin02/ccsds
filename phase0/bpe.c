f2a19228ce7ced144a74912203ccbb742b68c326
David Barina <ibarina@fit.vutbr.cz>
2019-05-29 16:34:06 +0200

    bpe: bpe_encode_block_by_index

--- phase0/bpe.c
+++ phase0/bpe.c
@@ -27,8 +27,40 @@ int bpe_decode_block(int *data, ptrdiff_t stride, struct bio *bio)
 	return 0;
 }
 
+size_t get_total_no_blocks(struct frame *frame)
+{
+	size_t height, width;
+
+	assert(frame);
+
+	height = ceil_multiple8(frame->height);
+	width  = ceil_multiple8(frame->width);
+
+	return height / 8 * width / 8;
+}
+
+int bpe_encode_block_by_index(struct frame *frame, struct bio *bio, size_t block_index)
+{
+	size_t height, width;
+	size_t y, x;
+	int *data;
+
+	assert(frame);
+
+	height = ceil_multiple8(frame->height);
+	width  = ceil_multiple8(frame->width);
+
+	y = block_index / (height / 8) * 8;
+	x = block_index % (height / 8) * 8;
+
+	data = frame->data;
+
+	return bpe_encode_block(data + y*width + x, width, bio);
+}
+
 int bpe_encode(struct frame *frame, const struct parameters *parameters, struct bio *bio)
 {
+#if 0
 	ptrdiff_t height, width;
 	ptrdiff_t y, x;
 	int *data;
@@ -45,7 +77,16 @@ int bpe_encode(struct frame *frame, const struct parameters *parameters, struct 
 			bpe_encode_block(data + y*width + x, width, bio);
 		}
 	}
+#else
+	size_t total_no_blocks;
+	size_t block_index;
 
+	total_no_blocks = get_total_no_blocks(frame);
+
+	for (block_index = 0; block_index < total_no_blocks; ++block_index) {
+		bpe_encode_block_by_index(frame, bio, block_index);
+	}
+#endif
 	return 0;
 }
 
