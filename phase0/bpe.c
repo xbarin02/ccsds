660b3bdb1aead910a882e41fdd5967a55acd37ed
David Barina <ibarina@fit.vutbr.cz>
2019-05-29 16:41:35 +0200

    bpe: bpe_decode_block_by_index, ptrdiff_t -> size_t

--- phase0/bpe.c
+++ phase0/bpe.c
@@ -1,9 +1,9 @@
 #include "bpe.h"
 #include <assert.h>
 
-int bpe_encode_block(int *data, ptrdiff_t stride, struct bio *bio)
+int bpe_encode_block(int *data, size_t stride, struct bio *bio)
 {
-	ptrdiff_t y, x;
+	size_t y, x;
 
 	for (y = 0; y < 8; ++y) {
 		for (x = 0; x < 8; ++x) {
@@ -14,9 +14,9 @@ int bpe_encode_block(int *data, ptrdiff_t stride, struct bio *bio)
 	return 0;
 }
 
-int bpe_decode_block(int *data, ptrdiff_t stride, struct bio *bio)
+int bpe_decode_block(int *data, size_t stride, struct bio *bio)
 {
-	ptrdiff_t y, x;
+	size_t y, x;
 
 	for (y = 0; y < 8; ++y) {
 		for (x = 0; x < 8; ++x) {
@@ -58,26 +58,27 @@ int bpe_encode_block_by_index(struct frame *frame, struct bio *bio, size_t block
 	return bpe_encode_block(data + y*width + x, width, bio);
 }
 
-int bpe_encode(struct frame *frame, const struct parameters *parameters, struct bio *bio)
+int bpe_decode_block_by_index(struct frame *frame, struct bio *bio, size_t block_index)
 {
-#if 0
-	ptrdiff_t height, width;
-	ptrdiff_t y, x;
+	size_t height, width;
+	size_t y, x;
 	int *data;
 
 	assert(frame);
 
-	height = (ptrdiff_t) ceil_multiple8(frame->height);
-	width  = (ptrdiff_t) ceil_multiple8(frame->width);
+	height = ceil_multiple8(frame->height);
+	width  = ceil_multiple8(frame->width);
+
+	y = block_index / (height / 8) * 8;
+	x = block_index % (height / 8) * 8;
 
 	data = frame->data;
 
-	for (y = 0; y < height; y += 8) {
-		for (x = 0; x < width; x += 8) {
-			bpe_encode_block(data + y*width + x, width, bio);
-		}
-	}
-#else
+	return bpe_decode_block(data + y*width + x, width, bio);
+}
+
+int bpe_encode(struct frame *frame, const struct parameters *parameters, struct bio *bio)
+{
 	size_t total_no_blocks;
 	size_t block_index;
 
@@ -86,27 +87,19 @@ int bpe_encode(struct frame *frame, const struct parameters *parameters, struct 
 	for (block_index = 0; block_index < total_no_blocks; ++block_index) {
 		bpe_encode_block_by_index(frame, bio, block_index);
 	}
-#endif
+
 	return 0;
 }
 
 int bpe_decode(struct frame *frame, const struct parameters *parameters, struct bio *bio)
 {
-	ptrdiff_t height, width;
-	ptrdiff_t y, x;
-	int *data;
+	size_t total_no_blocks;
+	size_t block_index;
 
-	assert(frame);
+	total_no_blocks = get_total_no_blocks(frame);
 
-	height = (ptrdiff_t) ceil_multiple8(frame->height);
-	width  = (ptrdiff_t) ceil_multiple8(frame->width);
-
-	data = frame->data;
-
-	for (y = 0; y < height; y += 8) {
-		for (x = 0; x < width; x += 8) {
-			bpe_decode_block(data + y*width + x, width, bio);
-		}
+	for (block_index = 0; block_index < total_no_blocks; ++block_index) {
+		bpe_decode_block_by_index(frame, bio, block_index);
 	}
 
 	return 0;
