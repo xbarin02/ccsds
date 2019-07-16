12a69ff09f459646e354096308e7f0989b24812b
David Barina <ibarina@fit.vutbr.cz>
2019-07-16 17:32:57 +0200

    bpe: struct bpe, bpe_push_block

--- phase0/bpe.c
+++ phase0/bpe.c
@@ -1,5 +1,42 @@
 #include "bpe.h"
 #include <assert.h>
+#include <stdlib.h>
+
+struct bpe {
+	size_t S;
+
+	/* S blocks (the S is given in struct parameters) */
+	/* i.e. Sx8x8 32-bit integers */
+	INT32 *segment;
+
+	size_t block_index;
+
+	struct bio *bio;
+};
+
+int bpe_init(struct bpe *bpe, const struct parameters *parameters, struct bio *bio)
+{
+	size_t S;
+
+	assert(bpe);
+	assert(parameters);
+
+	S = parameters->S;
+
+	bpe->segment = malloc( S * 8 * 8 * sizeof(INT32) );
+
+	if (bpe->segment == NULL) {
+		return RET_FAILURE_MEMORY_ALLOCATION;
+	}
+
+	bpe->S = S;
+
+	bpe->bio = bio;
+
+	bpe->block_index = 0;
+
+	return RET_SUCCESS;
+}
 
 int bpe_encode_block(int *data, size_t stride, struct bio *bio)
 {
@@ -12,6 +49,62 @@ int bpe_encode_block(int *data, size_t stride, struct bio *bio)
 	}
 
 	return 0;
+}
+
+int bpe_push_block(struct bpe *bpe, INT32 *data, size_t stride)
+{
+	size_t S;
+	size_t s;
+	INT32 *local;
+	size_t y, x;
+
+	assert(bpe);
+
+	/* push block into bpe->segment[] at the index (block_index%S) */
+
+	S = bpe->S;
+	s = bpe->block_index % S;
+	local = bpe->segment + s*8*8;
+
+	for (y = 0; y < 8; ++y) {
+		for (x = 0; x < 8; ++x) {
+			local[y*8 + x] = (UINT32) data[y*stride + x]; /* FIXME int -> UINT32 */
+		}
+	}
+
+	/* if this is the last block in the segment, serialize the segment into bpe->bio */
+	if (s + 1 == S) {
+		size_t blk;
+		/* encode this segment */
+		dprint (("BPE: encoding segment %lu\n", (bpe->block_index / S)));
+
+		for (blk = 0; blk < S; ++blk) {
+			/* encode the block */
+			bpe_encode_block(bpe->segment + blk*8*8, 8, bpe->bio);
+		}
+	}
+
+	/* next block will be... */
+	bpe->block_index ++;
+}
+
+int bpe_flush(struct bpe *bpe)
+{
+	size_t S;
+	size_t s;
+
+	S = bpe->S;
+	s = bpe->block_index % S;
+
+	if (s > 0) {
+		size_t blk;
+
+		dprint (("BPE: flushing %lu blocks\n", s));
+
+		for (blk = 0; blk < s; ++blk) {
+			bpe_encode_block(bpe->segment + blk*8*8, 8, bpe->bio);
+		}
+	}
 }
 
 int bpe_decode_block(int *data, size_t stride, struct bio *bio)
@@ -39,7 +132,12 @@ size_t get_total_no_blocks(struct frame *frame)
 	return height / 8 * width / 8;
 }
 
-int bpe_encode_block_by_index(struct frame *frame, struct bio *bio, size_t block_index)
+struct block {
+	int *data;
+	size_t stride;
+};
+
+int block_by_index(struct block *block, struct frame *frame, size_t block_index)
 {
 	size_t height, width;
 	size_t y, x;
@@ -50,35 +148,40 @@ int bpe_encode_block_by_index(struct frame *frame, struct bio *bio, size_t block
 	height = ceil_multiple8(frame->height);
 	width  = ceil_multiple8(frame->width);
 
-	y = block_index / (height / 8) * 8;
-	x = block_index % (height / 8) * 8;
+	y = block_index / (width / 8) * 8;
+	x = block_index % (width / 8) * 8;
 
 	data = frame->data;
 
-	return bpe_encode_block(data + y*width + x, width, bio);
+	assert(block);
+
+	block->data = data + y*width + x;
+	block->stride = width;
+
+	return 0;
+}
+
+int bpe_encode_block_by_index(struct frame *frame, struct bio *bio, size_t block_index)
+{
+	struct block block;
+
+	block_by_index(&block, frame, block_index);
+
+	return bpe_encode_block(block.data, block.stride, bio);
 }
 
 int bpe_decode_block_by_index(struct frame *frame, struct bio *bio, size_t block_index)
 {
-	size_t height, width;
-	size_t y, x;
-	int *data;
+	struct block block;
 
-	assert(frame);
+	block_by_index(&block, frame, block_index);
 
-	height = ceil_multiple8(frame->height);
-	width  = ceil_multiple8(frame->width);
-
-	y = block_index / (height / 8) * 8;
-	x = block_index % (height / 8) * 8;
-
-	data = frame->data;
-
-	return bpe_decode_block(data + y*width + x, width, bio);
+	return bpe_decode_block(block.data, block.stride, bio);
 }
 
 int bpe_encode(struct frame *frame, const struct parameters *parameters, struct bio *bio)
 {
+#if 0
 	size_t total_no_blocks;
 	size_t block_index;
 	size_t S;
@@ -97,6 +200,26 @@ int bpe_encode(struct frame *frame, const struct parameters *parameters, struct 
 	}
 
 	return 0;
+#else
+	size_t block_index;
+	size_t total_no_blocks;
+	struct bpe bpe;
+
+	total_no_blocks = get_total_no_blocks(frame);
+
+	bpe_init(&bpe, parameters, bio);
+
+	/* push all blocks into the BPE engine */
+	for (block_index = 0; block_index < total_no_blocks; ++block_index) {
+		struct block block;
+
+		block_by_index(&block, frame, block_index);
+
+		bpe_push_block(&bpe, block.data, block.stride);
+	}
+
+	bpe_flush(&bpe);
+#endif
 }
 
 int bpe_decode(struct frame *frame, const struct parameters *parameters, struct bio *bio)
