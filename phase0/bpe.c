e5af0ba74451f9e7e36a84acd171f2fe8e067fd3
David Barina <ibarina@fit.vutbr.cz>
2019-09-20 16:55:26 +0200

    bpe: cleanup

--- phase0/bpe.c
+++ phase0/bpe.c
@@ -694,12 +694,10 @@ int bpe_push_block(struct bpe *bpe, INT32 *data, size_t stride, size_t total_no_
 	return RET_SUCCESS;
 }
 
-int bpe_pop_block_decode(struct bpe *bpe, INT32 *data, size_t stride, size_t total_no_blocks)
+int bpe_pop_block_decode(struct bpe *bpe, size_t total_no_blocks)
 {
 	size_t S;
 	size_t s;
-	INT32 *local;
-	size_t y, x;
 
 	S = bpe->S;
 
@@ -712,23 +710,11 @@ int bpe_pop_block_decode(struct bpe *bpe, INT32 *data, size_t stride, size_t tot
 	if (s == 0) {
 		bpe_decode_segment(bpe, total_no_blocks);
 	}
-#if 0
-	/* pop the block from bpe->segment[] */
-	local = bpe->segment + s*8*8;
 
-	/* access frame->data[] */
-	for (y = 0; y < 8; ++y) {
-		for (x = 0; x < 8; ++x) {
-			data[y*stride + x] = local[y*8 + x];
-		}
-	}
-
-	bpe->block_index ++;
-#endif
 	return RET_SUCCESS;
 }
 
-int bpe_pop_block_copy_data(struct bpe *bpe, INT32 *data, size_t stride, size_t total_no_blocks)
+int bpe_pop_block_copy_data(struct bpe *bpe, INT32 *data, size_t stride)
 {
 	size_t S;
 	size_t s;
@@ -741,12 +727,7 @@ int bpe_pop_block_copy_data(struct bpe *bpe, INT32 *data, size_t stride, size_t 
 	if (S != 0) {
 		s = bpe->block_index % S;
 	}
-#if 0
-	/* if this is the first block in the segment, deserialize it */
-	if (s == 0) {
-		bpe_decode_segment(bpe, total_no_blocks);
-	}
-#endif
+
 	/* pop the block from bpe->segment[] */
 	local = bpe->segment + s*8*8;
 
@@ -901,12 +882,12 @@ int bpe_decode(struct frame *frame, const struct parameters *parameters, struct 
 	for (block_index = 0; block_index < total_no_blocks; ++block_index) {
 		struct block block;
 
-		/* NOTE: the bpe_pop_block reallocates frame->data[], thus passing block.data causes access to invalid memory area */
-		bpe_pop_block_decode(&bpe, block.data, block.stride, total_no_blocks);
+		/* NOTE: the bpe_pop_block_decode reallocates frame->data[] */
+		bpe_pop_block_decode(&bpe, total_no_blocks);
 
 		block_by_index(&block, frame, block_index);
 
-		bpe_pop_block_copy_data(&bpe, block.data, block.stride, total_no_blocks);
+		bpe_pop_block_copy_data(&bpe, block.data, block.stride);
 	}
 
 	bpe_destroy(&bpe);
