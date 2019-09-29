aabc403bb1b3fdbd4b344f69afb8726896412a76
David Barina <ibarina@fit.vutbr.cz>
2019-09-29 21:03:29 +0200

    bpe: fixed the last commits

--- phase0/bpe.c
+++ phase0/bpe.c
@@ -1053,6 +1053,12 @@ static void map_quantized_dcs_to_mapped_quantized_dcs(struct bpe *bpe, size_t N)
 		INT32 x_max = +((INT32)1 << (N-1)) - 1;
 		UINT32 theta = uint32_min((UINT32)(quantized_dc[m-1] - x_min), (UINT32)(x_max - quantized_dc[m-1]));
 
+		/* NOTE see also CCSDS 121.0-B-2 */
+#if 1
+		assert(quantized_dc[m] <= x_max);
+		assert(quantized_dc[m] >= x_min);
+#endif
+
 		assert(quantized_dc[m-1] - x_min >= 0);
 		assert(x_max - quantized_dc[m-1] >= 0);
 
@@ -1087,12 +1093,6 @@ static void map_mapped_quantized_dcs_to_quantized_dcs(struct bpe *bpe, size_t N)
 		INT32 x_min = -((INT32)1 << (N-1));
 		INT32 x_max = +((INT32)1 << (N-1)) - 1;
 		UINT32 theta = uint32_min((UINT32)(quantized_dc[m-1] - x_min), (UINT32)(x_max - quantized_dc[m-1]));
-
-		/* NOTE see also CCSDS 121.0-B-2 */
-#if 0
-		assert(quantized_dc[m] <= x_max);
-		assert(quantized_dc[m] >= x_min);
-#endif
 
 		assert(quantized_dc[m-1] - x_min >= 0);
 		assert(x_max - quantized_dc[m-1] >= 0);
